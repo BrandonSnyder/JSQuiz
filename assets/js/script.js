426e1177641900431f706a2c3af4985de3ed197b
Brandon Snyder <brandon.snyder019@gmail.com>
2022-03-29 15:47:24 -0400

    commit to meet minimal requirements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,13 @@ var topContainer= document.querySelector(".top-container");
 var ulEl= document.getElementById("UL");
 var playerInitialsArray = [];
 var playerScoreArray= [];
-var highScoresOB={
-  playerInitialsArray:[],
-    playerScoreArray: []
-}
+// score as a variable
+var score = 0;
+// Attempt to store in object
+// var highScoresOB={
+//   playerInitialsArray:[],
+//     playerScoreArray: []
+// }
 
 var questionEL= document.getElementById("question");
 var answerAEL= document.getElementById("answerA");
@@ -25,59 +28,53 @@ var answerCEL= document.getElementById("answerC");
 var answerDEL= document.getElementById("answerD");
 var correctAnswerEl= document.getElementById("correctAnswer");
 
-
+if(window.location=="http://127.0.0.1:5500/04Homework/JSQuiz/index.html"){
 startBTN.addEventListener("click", startQuiz);
+}
 
 
 // Questions Array
 var questions = [
     {question: "Which of the following is not apart of the box content model?",
-    A: "Padding",
-    B: "Border",
-    C: "Margin",
-    D: "Class",
+    A: "A: Padding",
+    B: "B: Border",
+    C: "C: Margin",
+    D: "D: Class",
     correctAnswer: "Class"},
     {question: "Java Script is a _______ language?",
-    A: "Object-Oriented",
-    B: "Object-based",
-    C: "Procedural",
-    D: "none of the above",
+    A: "A:Object-Oriented",
+    B: "B: Object-based",
+    C: "C: Procedural",
+    D: "D: none of the above",
     correctAnswer: "Object-Oriented"},
     {question: "Which of the following methods is usd to access HTML element using JavaScript",
-    A: "getElementByID() ",
-    B: "getElementsByClassName() ",
-    C: "Both A and B",
-    D: "None of the above ",
+    A: "A: getElementByID() ",
+    B: "B: getElementsByClassName() ",
+    C: "C: Both A and B",
+    D: "D: None of the above ",
     correctAnswer: "Both A and B"},
     {question: "Which function is used to serialize an object into a JSON string in JavaScript?",
-    A: "stringify()",
-    B: "parse()",
-    C: "convert()",
-    D: "None of the above()",
+    A: "A: stringify()",
+    B: "B: parse()",
+    C: "C: convert()",
+    D: "D: None of the above()",
     correctAnswer:"stringify()"},
     {question: "How to stop an interval timer in Javascript?",
-    A: "clearInterval",
-    B: "Stop",
-    C: "clearTimer",
-    D: "intervalOver",
+    A: "A: clearInterval",
+    B: "B: Stop",
+    C: "C: clearTimer",
+    D: "D: intervalOver",
     correctAnswer: "clearInterval"},
 ];
 
 // Start quiz
 function startQuiz(){
-  
   i=0;
-  // hide original text
-  if(hideStart.style.display === 'none'){
-    hideStart.style.display = "block";
-    hiddenQ.style.display = "none";
-  }else{
-    hideStart.style.display = "none";
-    hiddenQ.style.display = "block";
-  };
-  // start time
+  hideStart.style.display= "none";
+  highScoresBTN.style.display="none";
+    // start time
   setTime();
-  displayQuestion();
+  
 
 }
   // display question
@@ -88,33 +85,37 @@ function startQuiz(){
   answerBEL.addEventListener("click", checkAnswer);
   answerCEL.addEventListener("click", checkAnswer);
   answerDEL.addEventListener("click", checkAnswer);
-    if(i<questions.length){
+    
+  if(i<questions.length){
   questionEL.textContent= questions[i].question;
   answerAEL.textContent= questions[i].A;
   answerBEL.textContent= questions[i].B;
   answerCEL.textContent= questions[i].C;
   answerDEL.textContent= questions[i].D;
     }else{
-      displayScore()
-    }
+      hiddenQ.style.display = "none";
+      score=secondsLeft;
+      // window.location.href="http://127.0.0.1:5500/04Homework/JSQuiz/highscores.html"
+      return true
+    } 
   }  
 
 // This function will prompt user of initials and store them on the High Scores pages where the data is stored in local storage. 
 function displayScore(){
  
   var playerInitials = window.prompt("Enter your initials", "INI");
+console.log(playerInitials + " got a score of " + score);
 
   highScoresOB = JSON.parse(localStorage.getItem("highScoreStorage"));
 
 if(highScoresOB!== null){
-
   highScoresOB.playerInitialsArray.push(playerInitials);
   highScoresOB.playerScoreArray.push(secondsLeft);
   localStorage.setItem("highScoreStorage", JSON.stringify(highScoresOB))
   }
- console.log(highScoresOB)
+ console.log(highScoresOB);
+ 
 
- window.location.href =  "./highscores.html";
 
 }
 
@@ -124,11 +125,15 @@ function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function() {
       secondsLeft--;
+      hiddenQ.style.display = "flex"
+      var done = displayQuestion();
       timer.textContent = secondsLeft;
-
-      if(secondsLeft === 0) {
+      
+      if(secondsLeft === 0 || done) {
         // Stops execution of action at set interval
+        displayScore()
         clearInterval(timerInterval);
+        window.location.assign("http://127.0.0.1:5500/04Homework/JSQuiz/highscores.html")
         // Calls function to create and append image
       }
     }, 1000);
